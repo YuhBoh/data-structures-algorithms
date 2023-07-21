06a89a279f55fcf04bc6423874e1795aa2dce849
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-07-20 23:05:58 -0700

    finished notes on Maps

---
+++ Data-Structures/5-Maps.js
@@ -0,0 +1,39 @@
+// Map: unordered collection of key-value pairs. Both can be any data type.
+
+//1. Use corresponding key to retrieve value.
+
+//2. Maps are iterable. Can be used with for of loops.
+
+// Object vs Map
+// Obj - unordered
+// Obj - keys can be string or symbol
+// Obj - default keys that can collide with own keys
+// Obj - not iterable
+// Obj - # of items must be determined manually
+// Obj - storing data + attach functionality 
+
+// Maps - ordered
+// Maps - any type
+// Maps - no keys by default
+// Maps - iterable
+// Maps - # of items available with size property
+// Maps - only for storing data
+
+// Map
+const map = new Map([['a', 1], ['b', 2]])
+// to add new value:
+map.set('c', 3)
+// to delete key value pair
+map.delete('c')
+
+console.log(map.has('a')) // bool
+
+// check # of key value pair
+console.log(map.size);
+
+// delete all key value pairs
+map.clear();
+
+for (const [key,value] of map) {
+  console.log(`${key}: ${value}`)
+}
